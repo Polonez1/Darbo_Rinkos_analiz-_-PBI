4366997b5e16dfcb352f862fe6d60b74f523d89e
Polonez1 <darjus.vasiukevic@gmail.com>
2023-11-17 02:00:10 +0200

    created versions unify module

---
+++ sel.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import re
+
+from selenium import webdriver
+from selenium_stealth import stealth
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.common.exceptions import JavascriptException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.common.exceptions import NoSuchElementException
+
+from bs4 import BeautifulSoup
+
+from random import randint
+
+import pickle
+import time
+from time import sleep
+
+
+class ScrapingEngine:
+    driver_path = "./chromedriver.exe"
+
+    def __init__(self) -> None:
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("start-maximized")
+        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        self.options.add_experimental_option("useAutomationExtension", False)
+        self.options.add_argument("--disable-notifications")
+        self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+        self.options.add_argument(f"user-agent={self.user_agent}")
+
+        chrome_service = ChromeService(executable_path=self.driver_path)
+        self.driver = webdriver.Chrome(service=chrome_service, options=self.options)
+        stealth(
+            self.driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+        )
