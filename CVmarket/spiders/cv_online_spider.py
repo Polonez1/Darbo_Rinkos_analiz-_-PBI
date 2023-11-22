35d9243827606ebbba43ec1637b8144422078e12
Polonez1 <darjus.vasiukevic@gmail.com>
2023-11-22 05:33:56 +0200

    updated spider

--- CVmarket/spiders/cv_online_spider.py
+++ CVmarket/spiders/cv_online_spider.py
@@ -49,10 +49,7 @@ class QuotesSpider(scrapy.Spider):
             new_string = string.partition(",")[0].strip()
             return new_string
 
-    def parse_link(self, response):
-        url_general = response.meta["general_url"]
-        self.selenium_engine.driver.get(response.url)
-        time.sleep(10)
+    def __close_pop_up(self):
         close = self.selenium_engine.driver.find_element(
             By.CSS_SELECTOR, "button[type='button'].jsx-4189752321.close-modal-button"
         )
@@ -65,6 +62,13 @@ class QuotesSpider(scrapy.Spider):
         if cookies:
             cookies.click()
 
+    def parse_link(self, response):
+        url_general = response.meta["general_url"]
+
+        self.selenium_engine.driver.get(response.url)
+        time.sleep(10)
+        self.__close_pop_up()
+
         html_source = self.selenium_engine.driver.page_source
 
         time.sleep(10)
@@ -74,63 +78,57 @@ class QuotesSpider(scrapy.Spider):
             body=html_source,
             encoding="utf-8",
         )
-        # self.selenium_engine.driver.close()
+        page = 1
+        while True:
+            time.sleep(5)
+            for item in new_response.css("li.vacancies-list__item.false"):
+                href = item.css("div.jsx-3024910437 a::attr(href)").get()
+                description = item.css(
+                    "div.jsx-3024910437 span.jsx-3024910437.vacancy-item__title::text"
+                ).get()
+                company = item.css(
+                    "div.jsx-3024910437 div.vacancy-item__body div.vacancy-item__column a::text"
+                ).get()
+                salary = item.css(
+                    "div.jsx-3024910437 div.vacancy-item__info-secondary span.vacancy-item__salary-label::text"
+                ).get()
+                salary_from = self.__salary_split(salary, split_part=0)
+                salary_to = self.__salary_split(salary, split_part=2)
+                _city = item.css(
+                    "div.jsx-3024910437 .vacancy-item__locations::text"
+                ).get()
+                city = self.__city_split(_city)
 
-        time.sleep(5)
-        for item in new_response.css("li.vacancies-list__item.false"):
-            href = item.css("div.jsx-3024910437 a::attr(href)").get()
-            description = item.css(
-                "div.jsx-3024910437 span.jsx-3024910437.vacancy-item__title::text"
-            ).get()
-            company = item.css(
-                "div.jsx-3024910437 div.vacancy-item__body div.vacancy-item__column a::text"
-            ).get()
-            salary = item.css(
-                "div.jsx-3024910437 div.vacancy-item__info-secondary span.vacancy-item__salary-label::text"
-            ).get()
-            salary_from = self.__salary_split(salary, split_part=0)
-            salary_to = self.__salary_split(salary, split_part=2)
-            _city = item.css("div.jsx-3024910437 .vacancy-item__locations::text").get()
-            city = self.__city_split(_city)
+                yield {
+                    "np": page,
+                    "url": url_general,
+                    "link": href,
+                    "description": description,
+                    "company": company,
+                    "salary_from": salary_from,
+                    "salary_to": salary_to,
+                    "salary_type": "Prieš mokesčius",
+                    "city": city,
+                }
 
-            yield {
-                # "np": page,
-                "url": url_general,
-                "link": href,
-                "description": description,
-                "company": company,
-                "salary_from": salary_from,
-                "salary_to": salary_to,
-                "salary_type": "Prieš mokesčius",
-                "city": city,
-            }
-
-        next_page_xpath = '//*[@id="__next"]/div[2]/div[2]/div/div[2]/div/div/nav[1]/button[8]/svg/path'
-
-        next_page = self.selenium_engine.driver.find_element(
-            By.CSS_SELECTOR, "button[aria-label='Next']"
-        )
-        print("NEXTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
-        next_page.click()
+            next_page_xpath = '//*[@id="__next"]/div[2]/div[2]/div/div[2]/div/div/nav[1]/button[8]/svg/path'
+            next_page = self.selenium_engine.driver.find_element(
+                By.CSS_SELECTOR, "button[aria-label='Next']"
+            )
+            if next_page and not next_page.get_attribute("disabled"):
+                next_page.click()
+                time.sleep(5)
+                page += 1
+                new_response = HtmlResponse(
+                    url=self.selenium_engine.driver.current_url,
+                    body=self.selenium_engine.driver.page_source,
+                    encoding="utf-8",
+                )
+            else:
+                break
 
         time.sleep(10)
         self.selenium_engine.driver.close()
-
-
-#
-# self.selenium_engine.driver.close()
-
-
-# next_page_url = urljoin(url_general, next_page)
-# print(next_page_url)
-# if next_page:
-#    yield scrapy.Request(
-#        url=next_page_url,
-#        callback=self.parse_link,
-#        meta={"general_url": url_general, "page": next_page},
-#    )
-
-# yield {"name": name, "url": url, "category_id": category_id}
 
 
 # scrapy crawl cv_online_link_spider -o output2.json
