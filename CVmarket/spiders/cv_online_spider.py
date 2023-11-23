3f33dfe3040b90f75ae8d71bee0803e5aedff5d6
Polonez1 <darjus.vasiukevic@gmail.com>
2023-11-24 01:49:34 +0200

    ,

--- CVmarket/spiders/cv_online_spider.py
+++ CVmarket/spiders/cv_online_spider.py
@@ -1,13 +1,22 @@
 import scrapy
-from urllib.parse import urljoin
 import time
 import sys
 from scrapy.http import HtmlResponse
+import logging
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+# selenium_logger.setLevel(logging.WARNING)
+logging.getLogger("scrapy").setLevel(logging.ERROR)
+logging.getLogger("selenium.webdriver").setLevel(logging.ERROR)
+logging.getLogger("websockets.protocol").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+logging.getLogger().setLevel(logging.ERROR)
 
 sys.path.append("./Selenium/")
 from scraping_engine import ScrapingEngine
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 class QuotesSpider(scrapy.Spider):
@@ -15,25 +24,9 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["https://www.cvonline.lt/lt/categories"]
     selenium_engine = ScrapingEngine()
 
-    def parse(self, response):
-        for a_element in response.css(
-            "div.jsx-3492702657.categories__vacancy_categories a"
-        ):
-            link = a_element.css("a::attr(href)").get()
-            name = a_element.css("a::text").get()
-            # yield {"name": name, "url": link}
-
-            if link:
-                full_url = response.urljoin(link)
-
-                yield scrapy.Request(
-                    url=full_url,
-                    callback=self.parse_link,
-                    meta={"name": name, "general_url": full_url},
-                )
-                break
-            else:
-                yield {"name": name, "url": link}
+    def __init__(self, *args, **kwargs):
+        super(QuotesSpider, self).__init__(*args, **kwargs)
+        self.parse_function = kwargs.get("parse_function", "parse")
 
     def __salary_split(self, string: str, split_part: int):
         if string is None:
@@ -49,29 +42,65 @@ class QuotesSpider(scrapy.Spider):
             new_string = string.partition(",")[0].strip()
             return new_string
 
-    def __close_pop_up(self):
-        close = self.selenium_engine.driver.find_element(
-            By.CSS_SELECTOR, "button[type='button'].jsx-4189752321.close-modal-button"
-        )
-        if close:
-            close.click()
+    def __close_pop_up(self, engine):
+        try:
+            close = self.selenium_engine.driver.find_element(
+                By.CSS_SELECTOR,
+                "button[type='button'].jsx-4189752321.close-modal-button",
+            )
+            if close:
+                close.click()
+        except:
+            pass
         time.sleep(5)
-        cookies = self.selenium_engine.driver.find_element(
-            By.CLASS_NAME, "cookie-consent-button"
-        )
-        if cookies:
-            cookies.click()
+        try:
+            cookies = self.selenium_engine.driver.find_element(
+                By.CLASS_NAME, "cookie-consent-button"
+            )
+            if cookies:
+                cookies.click()
+        except:
+            pass
+
+    def parse_category(self, response):
+        for a_element in response.css(
+            "div.jsx-3492702657.categories__vacancy_categories a"
+        ):
+            link = a_element.css("a::attr(href)").get()
+            name = a_element.css("a::text").get()
+            yield {"name": name, "url": link}
+
+    def parse(self, response):
+        for a_element in response.css(
+            "div.jsx-3492702657.categories__vacancy_categories a"
+        ):
+            link = a_element.css("a::attr(href)").get()
+            name = a_element.css("a::text").get()
+            # yield {"name": name, "url": link}
+            full_url = response.urljoin(link)
+            print("!!!!!!!!! ČIA PARSE FUNKCIJA RENKA DUOMENYS !!!!!!!!!!!!!!!!!!!")
+            yield scrapy.Request(
+                url=full_url,
+                callback=self.parse_link,
+                meta={"name": name, "general_url": full_url},
+            )
 
     def parse_link(self, response):
         url_general = response.meta["general_url"]
+        self.selenium_engine.driver.execute_script("window.open('', '_blank');")
+
+        # Przejdź do ostatniej otwartej zakładki
+        self.selenium_engine.driver.switch_to.window(
+            self.selenium_engine.driver.window_handles[-1]
+        )
 
         self.selenium_engine.driver.get(response.url)
-        time.sleep(10)
-        self.__close_pop_up()
+        time.sleep(5)
+        self.__close_pop_up(engine=self.selenium_engine.driver)
 
         html_source = self.selenium_engine.driver.page_source
 
-        time.sleep(10)
+        time.sleep(5)
 
         new_response = HtmlResponse(
             url=self.selenium_engine.driver.current_url,
@@ -110,25 +139,53 @@ class QuotesSpider(scrapy.Spider):
                     "salary_type": "Prieš mokesčius",
                     "city": city,
                 }
-
-            next_page_xpath = '//*[@id="__next"]/div[2]/div[2]/div/div[2]/div/div/nav[1]/button[8]/svg/path'
-            next_page = self.selenium_engine.driver.find_element(
-                By.CSS_SELECTOR, "button[aria-label='Next']"
-            )
-            if next_page and not next_page.get_attribute("disabled"):
-                next_page.click()
-                time.sleep(5)
-                page += 1
-                new_response = HtmlResponse(
-                    url=self.selenium_engine.driver.current_url,
-                    body=self.selenium_engine.driver.page_source,
-                    encoding="utf-8",
+            try:
+                next_page = self.selenium_engine.driver.find_element(
+                    By.CSS_SELECTOR, "button[aria-label='Next']"
                 )
+                print("!!!!SURASTAS NEXT MYGTUKAS!!!!")
+            except:
+                next_page = False
+                print("???????????NESURASTAS NEXT MYGTUKAS???????????????????")
+
+            if next_page:
+                if next_page and not next_page.get_attribute("disabled"):
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                    print(next_page)
+                    next_page.click()
+                    print(
+                        f"!!!!!!!!!!Laukimas prieš nusiuniant response!!!!!!!!!!!!!!!"
+                    )
+                    time.sleep(10)
+                    page += 1
+                    print(f"!!!!!!!!!!NUSPAUSTAS NEXT MYGTUKAS {page} PUSLAPIS")
+                    new_response = HtmlResponse(
+                        url=self.selenium_engine.driver.current_url,
+                        body=self.selenium_engine.driver.page_source,
+                        encoding="utf-8",
+                    )
+                else:
+                    print(
+                        "??????????CIKLAS NUTRAUKTAS dėl neaktyvaus mygtuko???????????????"
+                    )
+                    break
             else:
+                print(
+                    "??????????CIKLAS NUTRAUKTAS nes nesurastas mygtukas???????????????"
+                )
                 break
 
-        self.selenium_engine.driver.close()
+        self.selenium_engine.driver.execute_script("window.open('', '_blank');")
+
+        # Przejdź do ostatniej otwartej zakładki
+        self.selenium_engine.driver.switch_to.window(
+            self.selenium_engine.driver.window_handles[-1]
+        )
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, getattr(self, self.parse_function, self.parse))
 
 
-# scrapy crawl cv_online_link_spider -o cvonline.json
-# scrapy crawl cv_online_link_spider -o cvonline_category.json
+# scrapy crawl cv_online_link_spider -o ./output/cvonline.json
+# scrapy crawl cv_online_link_spider -o ./output/cvonline_category.json -a parse_function=parse_category
