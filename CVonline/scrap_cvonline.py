e04ede566fa521c3bfcd30dfa7e7182d792f2025
Polonez1 <darjus.vasiukevic@gmail.com>
2023-12-01 22:41:09 +0200

    created cvonline parser

--- CVonline/scrap_cvonline.py
+++ CVonline/scrap_cvonline.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+import time
+import json
 
 
 class CVonlineParse:
@@ -6,6 +8,26 @@ class CVonlineParse:
 
     def __init__(self):
         self.category_hrefs: dict = {}
+        self.full_data: list = []
+
+    def __salary_split(self, string: str, split_part: int):
+        if string is None:
+            return None
+        else:
+            new_string = string.partition("–")[split_part].replace("€", "").strip()
+            return new_string
+
+    def __city_split(self, string: str):
+        if string is None:
+            return None
+        else:
+            new_string = string.partition(",")[0].strip()
+            return new_string
+
+    def __output(self, data):
+        output_file_path = "./output/cvonline.json"
+        with open(output_file_path, "w", encoding="utf-8") as output_file:
+            json.dump(self.full_data, output_file, ensure_ascii=False, indent=2)
 
     def __collect_hrefs(self):
         category_block = self.page.wait_for_selector(".categories__vacancy_categories")
@@ -16,10 +38,68 @@ class CVonlineParse:
                 name = link.inner_text()
                 self.category_hrefs[name] = href
 
+    def __collect_vacancies(self, vacancies, url_general):
+        page = 1
+        while True:
+            time.sleep(2)
+            for vacancy in vacancies:
+                url_general = url_general
+                href = vacancy.query_selector(".vacancy-item").get_attribute("href")
+                description = (
+                    vacancy.query_selector(".vacancy-item__title").inner_text().strip()
+                )
+                company = (
+                    vacancy.query_selector(".vacancy-item__column a")
+                    .inner_text()
+                    .strip()
+                )
+                try:
+                    _salary = vacancy.eval_on_selector(
+                        ".vacancy-item__salary-label", "(element) => element.innerText"
+                    )
+                except:
+                    _salary = None
+                salary_from = self.__salary_split(_salary, 0)
+                salary_to = self.__salary_split(_salary, 2)
+
+                _city = vacancy.query_selector(".vacancy-item__locations").inner_text()
+                city = self.__city_split(_city)
+
+                data = {
+                    "np": page,
+                    "url": url_general,
+                    "link": href,
+                    "description": description,
+                    "company": company,
+                    "salary_from": salary_from,
+                    "salary_to": salary_to,
+                    "salary_type": "Prieš mokesčius",
+                    "city": city,
+                }
+                self.full_data.append(data)
+                print(page, description, salary_from, city)
+                # print(page, company, _salary)
+
+            next_button = self.page.query_selector("button[aria-label='Next']")
+            page += 1
+            if next_button:
+                if next_button.is_enabled():
+                    next_button.click()
+                    time.sleep(10)
+                    vacancies = self.page.query_selector_all(".vacancies-list__item")
+                else:
+                    break
+            else:
+                break
+
     def __parse_links(self):
         for key in self.category_hrefs.keys():
             name = key
             href = self.category_hrefs[key]
+            url = self.general_url + href
+            self.page.goto(url)
+            vacancies = self.page.query_selector_all(".vacancies-list__item")
+            self.__collect_vacancies(vacancies=vacancies, url_general=href)
 
     def parse(self):
         with sync_playwright() as playwright:
@@ -31,8 +111,10 @@ class CVonlineParse:
             self.page.get_by_label("Accept cookies").click()
             self.__collect_hrefs()
             self.__parse_links()
+            self.__output(data=self.full_data)
 
 
 if "__main__" == __name__:
     cv = CVonlineParse()
     cv.parse()
+    print(cv.full_data)
