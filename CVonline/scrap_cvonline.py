8426eb1431f03e16e2356a4260203e156f6ec6a6
Polonez1 <darjus.vasiukevic@gmail.com>
2023-12-01 02:41:00 +0200

    .

--- CVonline/scrap_cvonline.py
+++ CVonline/scrap_cvonline.py
@@ -2,19 +2,24 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 class CVonlineParse:
-    general_url: str = "https://www.cvonline.lt/"
+    general_url: str = "https://www.cvonline.lt"
 
     def __init__(self):
-        self.category_hrefs: list = []
+        self.category_hrefs: dict = {}
 
     def __collect_hrefs(self):
         category_block = self.page.wait_for_selector(".categories__vacancy_categories")
         if category_block:
-            # Teraz możesz pobrać linki z tego bloku
             links = category_block.query_selector_all("a")
             for link in links:
                 href = link.get_attribute("href")
-                self.category_hrefs.append(href)
+                name = link.inner_text()
+                self.category_hrefs[name] = href
+
+    def __parse_links(self):
+        for key in self.category_hrefs.keys():
+            name = key
+            href = self.category_hrefs[key]
 
     def parse(self):
         with sync_playwright() as playwright:
@@ -25,6 +30,7 @@ class CVonlineParse:
             self.page.locator(".jsx-4189752321").first.click()
             self.page.get_by_label("Accept cookies").click()
             self.__collect_hrefs()
+            self.__parse_links()
 
 
 if "__main__" == __name__:
