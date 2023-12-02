a7ad22582bf39ec9a7c4087007e54505a4b438e3
Polonez1 <darjus.vasiukevic@gmail.com>
2023-12-02 03:12:27 +0200

    update processing

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 cvmarket_name = "cvmarket"
-category_json_path = f"./{cvmarket_name}_category.json"
-CVmarket_json_path = f"./{cvmarket_name}.json"
+cvonline_name = "cvonline"
+
+
+data_path = "./output/"
+category_file = "./output/{name}_category.json"
+data_file = "./output/{name}.json"
--- processing.py
+++ processing.py
@@ -1,47 +1,79 @@
 import pandas as pd
 import json
-
+from urllib.parse import urlparse
 import config
 
 
 def _read_json(json_object: str):
     with open(json_object, "r", encoding="utf-8") as json_file:
         db = json.load(json_file)
-
     return db
 
 
-def _get_id_from_url(df: pd.DataFrame) -> pd.DataFrame:
-    df["category_id"] = df["url"].str.rpartition("-")[2]
-    return df
+class CVmarket:
+    cvmarket: str = config.cvmarket_name
+    cvmarket_category_file: str = config.category_file.format(name=cvmarket)
+    cvmarket_data: str = config.data_file.format(name=cvmarket)
+
+    def _get_id_from_url(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["category_id"] = df["url"].str.rpartition("-")[2]
+        return df
+
+    def category_processing(self) -> pd.DataFrame:
+        json_obj = _read_json(json_object=self.cvmarket_category_file)
+        df = pd.DataFrame(json_obj)
+        df = self._get_id_from_url(df)
+
+        return df
+
+    def cvmarket_processing(self):
+        json_obj = _read_json(json_object=self.cvmarket_data)
+        df = pd.DataFrame(json_obj)
+        df["source"] = self.cvmarket
+        df = self._get_id_from_url(df)
+
+        return df
 
 
-def _category_processing() -> pd.DataFrame:
-    json_obj = _read_json(json_object=config.category_json_path)
-    df = pd.DataFrame(json_obj)
-    df = _get_id_from_url(df)
+class CVonline:
+    cvonline: str = config.cvonline_name
+    cvonline_category_file: str = config.category_file.format(name=cvonline)
+    cvonline_data: str = config.data_file.format(name=cvonline)
 
-    return df
+    def _get_id_from_url(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["category_id"] = df["url"].str.rpartition("=")[2]
+        return df
 
+    def category_processing(self):
+        json_obj = _read_json(json_object=self.cvonline_category_file)
+        df = pd.DataFrame(json_obj)
+        df = self._get_id_from_url(df)
 
-def _CVmarket_processing():
-    json_obj = _read_json(json_object=config.CVmarket_json_path)
-    df = pd.DataFrame(json_obj)
-    df["source"] = "CVMarket"
-    df = _get_id_from_url(df)
+        return df
 
-    return df
+    def cvonline_processing(self):
+        json_obj = _read_json(json_object=self.cvonline_data)
+        df = pd.DataFrame(json_obj)
+        df["source"] = self.cvonline
+        df = self._get_id_from_url(df)
+
+        return df
 
 
 def processing_main():
-    category = _category_processing()
-    cvmarket = _CVmarket_processing()
+    cvm = CVmarket()
+    cvmarket_category = cvm.category_processing()
+    cvmarket_data = cvm.cvmarket_processing()
 
-    return category, cvmarket
+    cvo = CVonline()
+    cvonline_category = cvo.category_processing()
+    cvonline_data = cvo.cvonline_processing()
+
+    # cvmarket = _CVmarket_processing()
+
+    return cvonline_data
 
 
 if "__main__" == __name__:
-    cat, base = processing_main()
-
-    cat.to_excel("category.xlsx")
-    base.to_excel("cv_market.xlsx")
+    df = processing_main()
+    print(df)
